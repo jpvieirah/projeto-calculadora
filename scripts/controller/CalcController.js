e6d3092f99af88794e0ebaabc90dfddd3077f0ef
jpvieirah <jp201101@gmail.com>
2022-02-11 10:57:46 -0300

    ADD EVENT CLICK

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,8 @@ class CalcController {
     this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
+    this.initButtonsEvents();
+
 
 }
 
@@ -22,6 +24,22 @@ initialize(){
     }, 1000);
 
    
+
+}
+
+initButtonsEvents(){
+
+let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+buttons.forEach((btn, index) => {
+
+    btn.addEventListener('click', e => {
+
+        console.log(btn.className.baseVal.replace("btn-", ""));
+
+});
+
+})
 
 }
 
